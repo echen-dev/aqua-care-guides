5a7d9d593015f42ae313b3e2ed33fe9b307b4dc1
Ethan Chen <Ethanchen4@gmail.com>
2026-05-04 18:52:04 -0400

    Add navigation menu for desktop and mobile

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -2,7 +2,7 @@ const ContactPage = () => {
   return (
     <section>
       <h2 className="text-3xl font-bold text-white mb-8 text-center">
-        Contact Me
+        📬Contact Me
       </h2>
     </section>
   );
--- app/routes/guides/index.tsx
+++ app/routes/guides/index.tsx
@@ -1,7 +1,7 @@
 const GuidesPage = () => {
   return (
     <section>
-      <h2 className="text-3xl text-white font-bold mb-8">Care Guides</h2>
+      <h2 className="text-3xl text-white font-bold mb-8">📋Care Guides</h2>
     </section>
   );
 };
